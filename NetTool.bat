608ab2f35cf55e05653aa3e0eb33c57e5e31938c
mortza <79837660+mortzaLootTool@users.noreply.github.com>
2021-11-28 19:36:20 +0330

    Update NetTool.bat

--- NetTool.bat
+++ NetTool.bat
@@ -1,21 +1,26 @@
 ::Creator mortza
-::Contact To me in discord(mortza#3700)
+::Contact To me in discord( mortza#3700 )
 ::The Tool For Info or status net and ethernet
-::For Other Tool you need in(https://discord.gg/PASpgRWqSw)
+::For Other Tool you need in( https://discord.gg/PASpgRWqSw )
 @echo off
 ::-------------------------------------------------------
+
 
 
 ::Home
 :Home
 title NetTool
 color 0A
-mode con cols=128 lines=35
+set allowed_char_list="ABCDEFGHIJKLMNOPRSTUVYZWXQabcdefghijklmnoprstuvyzwxq0123456789-_"
+
+
 
 ::-------------------------------------------------------
 
 
-cls
+
+::Coming Soon...
+
 
 
 ::-------------------------------------------------------
@@ -54,12 +59,39 @@ echo.
 ::-------------------------------------------------------
 
 
+
+::Get Name
+:GetName
+mode con cols=40lines=10
+color 0A
+echo.
+echo  What is your name?
+echo.
+set /p name=">"
+cls
+echo.
+echo %name% is taken.
+echo Pls Enter a Key For Start Application.
+echo.
+set name=%name%
+pause 
+goto :main
+
+
+
+::-------------------------------------------------------
+
+
+
 ::main menu
+:main
+cls
+mode con cols=128 lines=35
 echo                                     -------------------------------------------------      
 echo                                     ^|           ---- +-+-+-+-+-+-+-+ ----           ^|
-echo                                     ^|                  -NetTool-                    ^|
+echo                                     ^|                   -NetTool-                   ^|
 echo                                     ^|           ---- +-+-+-+-+-+-+-+ ----           ^|
-echo                                     ^|                Welcome %username%!                  ^|
+echo                                     ^|                Welcome %name%!                ^|
 echo                                    -----------------------------------------------------      
 echo                                     ^|WHAT YOU NEED? Select it!(Press 1 to 10 option)^|    
 echo                                  ---------------------------------------------------------                                                   
@@ -81,27 +113,28 @@ echo                               ^| 7)Wlan(show Wirelless/Connect/Disconnect..
 echo.
 echo                               ^| 8)Choose _color_                                         ^|
 echo.
-echo                               ^| 9)About                                                 ^|
+echo                               ^| 9)About                                                  ^|
 echo. 
 echo                               ^| 10)exit                                                  ^|
 echo.
 echo                              ---------------------------------------------------------------
+echo.
 
-:main
-                                      set /p c=Select option:
-                                                            if /I "%c%" EQU "1" goto :IP_info
-                                                            if /I "%c%" EQU "2" goto :status 
-                                                            if /I "%c%" EQU "3" goto :ETH
-                                                            if /I "%c%" EQU "4" goto :CheckPort
-                                                            if /I "%C%" EQU "5" goto :ping
-                                                            if /I "%c%" EQU "6" goto :mac
-                                                            if /I "%c%" EQU "7" goto :wlan 
-                                                            if /I "%c%" EQU "8" goto :color
-                                                            if /I "%c%" EQU "9" goto :About
-                                                            if /I "%c%" EQU "10" goto exit
-                                                          echo "%c%" is not valid pls select 1 to 6 option!
-                                                          goto :main
-                                                          pause >NUL
+                                set /p c=Select option:
+if /I "%c%" EQU "1" goto :IP_info
+if /I "%c%" EQU "2" goto :status 
+if /I "%c%" EQU "3" goto :ETH
+if /I "%c%" EQU "4" goto :CheckPort
+if /I "%C%" EQU "5" goto :ping
+if /I "%c%" EQU "6" goto :mac
+if /I "%c%" EQU "7" goto :wlan 
+if /I "%c%" EQU "8" goto :color
+if /I "%c%" EQU "9" goto :About
+if /I "%c%" EQU "10" goto exit
+                    echo "%c%" is not valid pls select 1 to 6 option!
+goto :main
+pause >NUL
+
 
 
 ::-------------------------------------------------------
@@ -120,7 +153,7 @@ echo.
 and Other information Help you...
 ipconfig /all
 pause
-goto :Home
+goto :main
 
 
 
@@ -141,11 +174,11 @@ if errorlevel 1 goto :error
 :noerror 
 echo YES YOU HAVE NET! :)
 pause
-goto :Home
+goto :main
 :error
 echo no you dont have net :/
 pause
-goto :Home
+goto :main
 
 
 
@@ -169,7 +202,7 @@ echo.
 set /p ETH= Select:
 if '%ETH%' == '1' goto :Select1
 if '%ETH%' == '2' goto :Select2
-if '%ETH%' == '3' goto :Home
+if '%ETH%' == '3' goto :main
 echo "%ETH%" is not a valid option. Please, try again. 
 echo.
 
@@ -187,7 +220,7 @@ goto end
 :end
 pause
 goto ETH
-goto :Home
+goto :main
 
 
 
@@ -205,7 +238,7 @@ echo please wait ...
 netstat -ano
 tasklist|findstr "9999"
 pause 
-goto :Home
+goto :main
 
 
 
@@ -219,11 +252,11 @@ goto :Home
 cls
 mode con cols=60 lines=20
 echo ping From Host(8.8.8.8)or(1.1.1.1)?
-echo 1) 8.8.8.8(Google DNS)
+echo 1) 8.8.8.8(Google DNS) (Has Problem I fix it Soon...)
 echo 2) 1.1.1.1(one dns faster)
 
-echo Select: 1
-echo Select: 2
+echo Select: 1 To toggle google dns
+echo Select: 2 To Toggle One Dns 
 set /p pong= Select:
 if '%pong% == '1' goto :Select1
 if '%pong%'== '2' goto :Select2
@@ -236,24 +269,25 @@ goto :ping
 echo you ping is:
 echo to cansel it (Control+c) 
 ping -t 8.8.8.8
-echo to Back in menu pls select " N "
 pause 
-goto :Home
+goto :main
 :Select2
 echo you ping is:
 echo to cansel it (Control+c) 
 ping -t 1.1.1.1
 pause 
-goto :Home
+goto :main
 
 
 
 ::-------------------------------------------------------
 
 
+
 ::6)
 ::getmac address
 :mac
+mode con cols=100 lines=50
 echo ok!
 cls
 wmic nic get MACAddress,description
@@ -261,7 +295,8 @@ echo and more information is...
 echo Loading...
 GETMAC /S system /U user
 pause
-goto :Home
+goto :main
+
 
 
 ::-------------------------------------------------------
@@ -298,20 +333,21 @@ echo                        ^|    8)Password Creator (for wifi)                 
 echo.                       ^|                                                      ^|  
 echo.                       ^|    9)Return to main menu                             ^|
 echo                 ^|--------------------------------------------------------------------^| 
+
 :mainWifi
-                                      set /p w=Select option:
-                                        if /I "%w%" EQU "1" goto :1
-                                        if /I "%w%" EQU "2" goto :2
-                                        if /I "%w%" EQU "3" goto :3
-                                        if /I "%w%" EQU "4" goto :4
-                                        if /I "%w%" EQU "5" goto :5
-                                        if /I "%w%" EQU "6" goto :6
-                                        if /I "%w%" EQU "7" goto :7 
-                                        if /I "%w%" EQU "8" goto :8
-                                        if /I "%w%" EQU "9" goto :9
-                                        echo "%w%" is not valid pls select 1 to 8 option!
-                                        goto :Home
-                                        pause >NUL
+                         set /p w=Select option:
+if /I "%w%" EQU "1" goto :1
+if /I "%w%" EQU "2" goto :2
+if /I "%w%" EQU "3" goto :3
+if /I "%w%" EQU "4" goto :4
+if /I "%w%" EQU "5" goto :5
+if /I "%w%" EQU "6" goto :6
+if /I "%w%" EQU "7" goto :7 
+if /I "%w%" EQU "8" goto :8
+if /I "%w%" EQU "9" goto :9
+                         echo "%w%" is not valid pls select 1 to 8 option!
+goto :main
+pause >NUL
 
 :1
 cls
@@ -392,9 +428,9 @@ mode con cols=80 lines=20
 echo I will Added random pass with a Key(Char password and other pass you need..)
 echo pls say how many you need?(Type 1,5 or 10 and enter)
 echo -----------------------------------
-echo  1 Random Password
-echo  5 Random Passwords
-echo  10 Random Passwords
+echo  1)1 Random Password
+echo  2)5 Random Passwords
+echo  3)10 Random Passwords
 echo -----------------------------------
 echo and to back main wifi(press 0 and enter)
 set pass=
@@ -413,6 +449,8 @@ goto :wlan
 cls
 echo Here You Need!
 echo Your 5 passwords are %random%, %random%, %random%, %random%, %random%.
+pause
+goto :wlan
 :8-3
 cls
 echo Here You Need!
@@ -425,15 +463,14 @@ goto :wlan
 
 :9
 cls
-goto :Home
-
+goto :main
 
 
 
 ::-------------------------------------------------------
-
+::Coming Soon...
 ::-------------------------------------------------------
-
+::Coming Soon...
 ::-------------------------------------------------------
 
 
@@ -459,41 +496,42 @@ if /I "%cl%" EQU "r" goto :r
 if /I "%cl%" EQU "p" goto :p
 if /I "%cl%" EQU "g" goto :g
 if /I "%cl%" EQU "w" goto :w
-if /I "%cl%" EQU "home" goto :home
+if /I "%cl%" EQU "home" goto :main
 echo "%cl%" is invalid please try again!
 goto :clMain
 
 :b
 cls
 color 1
-goto :Home
+goto :main
 :a
 cls
 color 3
-goto :Home
+goto :main
 :r
 cls
 color 4
-goto :Home
+goto :main
 :p
 cls
 color 5
-goto :Home
+goto :main
 :g
 cls
 color 6
-goto :Home
+goto :main
 :w
 cls
 color 7
-goto :Home
+goto :main
 
 :home
-goto:Home
+goto :main
 
 
 
 ::-------------------------------------------------------
+
 
 
 ::9)
@@ -518,6 +556,9 @@ echo.
 echo        ^|----------------------------------------------------^|
 echo.
 pause 
-goto :Home
-          
+goto :main
+
+
+
+::-------------------------------------------------------
           
