fd6aafe2d868f0d9d9374ccb3f3dc97a0e7e57ee
mortza <79837660+mortzaLootTool@users.noreply.github.com>
2021-11-28 09:35:38 +0330

    Update NetTool.bat

--- NetTool.bat
+++ NetTool.bat
@@ -1,6 +1,7 @@
 ::Creator mortza
 ::Contact To me in discord(mortza#3700)
 ::The Tool For Info or status net and ethernet
+::For Other Tool you need in(https://discord.gg/PASpgRWqSw)
 @echo off
 ::-------------------------------------------------------
 
@@ -80,11 +81,9 @@ echo                               ^| 7)Wlan(show Wirelless/Connect/Disconnect..
 echo.
 echo                               ^| 8)Choose _color_                                         ^|
 echo.
-echo                               ^| 9)Full System Hardwar(With Bios!)                        ^|
-echo.
-echo                               ^| 10)About                                                 ^|
+echo                               ^| 9)About                                                 ^|
 echo. 
-echo                               ^| 11)exit                                                  ^|
+echo                               ^| 10)exit                                                  ^|
 echo.
 echo                              ---------------------------------------------------------------
 
@@ -99,7 +98,7 @@ echo                              ----------------------------------------------
                                                             if /I "%c%" EQU "7" goto :wlan 
                                                             if /I "%c%" EQU "8" goto :color
                                                             if /I "%c%" EQU "9" goto :About
-                                                            if /I "%c%" EQU "11" goto exit
+                                                            if /I "%c%" EQU "10" goto exit
                                                           echo "%c%" is not valid pls select 1 to 6 option!
                                                           goto :main
                                                           pause >NUL
@@ -257,6 +256,8 @@ goto :Home
 :mac
 echo ok!
 cls
+wmic nic get MACAddress,description
+echo and more information is...
 echo Loading...
 GETMAC /S system /U user
 pause
@@ -276,18 +277,26 @@ echo                                     ^|---- +-+-+-+-+-+-+-+ ----^|
 echo                                    ^|      -    Wlan    -       ^|
 echo                                     ^|---- +-+-+-+-+-+-+-+ ----^|
 echo.
-echo                      ------------------------------------------------------------
-echo                                            What you need?
+echo                      --------------------------------------------------------------
+echo                        ^|                *What you need?*                      ^|
 echo.                       ^|                                                      ^|
 echo                        ^|    1)Show all network(display wifi list)             ^|
+echo.                       ^|                                                      ^|
 echo                        ^|    2)show Properties wlan/lan drivers on system      ^|
+echo.                       ^|                                                      ^|
 echo                        ^|    3)show the allowed and bloched network list       ^|
+echo.                       ^|                                                      ^|
 echo                        ^|    4)show interface list of in system                ^|
+echo.                       ^|                                                      ^|
 echo                        ^|    5)show settings (global settings of wireless LAN) ^|
+echo.                       ^|                                                      ^|
 echo                        ^|    6)show tracing                                    ^|
+echo.                       ^|                                                      ^|
 echo                        ^|    7)show wireless capabilities                      ^|
-echo                        ^|                                                      ^|
-echo.                       ^|                8)Return to main menu                 ^|
+echo.                       ^|                                                      ^|
+echo                        ^|    8)Password Creator (for wifi)                     ^|
+echo.                       ^|                                                      ^|  
+echo.                       ^|    9)Return to main menu                             ^|
 echo                 ^|--------------------------------------------------------------------^| 
 :mainWifi
                                       set /p w=Select option:
@@ -299,6 +308,7 @@ echo                 ^|---------------------------------------------------------
                                         if /I "%w%" EQU "6" goto :6
                                         if /I "%w%" EQU "7" goto :7 
                                         if /I "%w%" EQU "8" goto :8
+                                        if /I "%w%" EQU "9" goto :9
                                         echo "%w%" is not valid pls select 1 to 8 option!
                                         goto :Home
                                         pause >NUL
@@ -378,6 +388,42 @@ cls
 goto :wlan
 
 :8
+mode con cols=80 lines=20
+echo I will Added random pass with a Key(Char password and other pass you need..)
+echo pls say how many you need?(Type 1,5 or 10 and enter)
+echo -----------------------------------
+echo  1 Random Password
+echo  5 Random Passwords
+echo  10 Random Passwords
+echo -----------------------------------
+echo and to back main wifi(press 0 and enter)
+set pass=
+set /p pass= Choice: 
+if %pass%==1 goto 8-1 if NOT goto wlan
+if %pass%==2 goto 8-2 if NOT goto wlan
+if %pass%==3 goto 8-3 if NOT goto wlan
+if %pass%==0 goto 8-0 if NOT goto wlan
+:8-1
+cls
+echo Here You Need!
+echo Your password is %random%
+pause
+goto :wlan
+:8-2
+cls
+echo Here You Need!
+echo Your 5 passwords are %random%, %random%, %random%, %random%, %random%.
+:8-3
+cls
+echo Here You Need!
+echo Your 10 Passwords are %random%, %random%, %random%, %random%, %random%, %random%, %random%, %random%, %random%, %random%
+goto :wlan
+:8-0
+cls
+echo ok!
+goto :wlan
+
+:9
 cls
 goto :Home
 
@@ -466,9 +512,12 @@ echo.
 echo             -+- Contact To me In Discord(mortza#3700) -+-
 echo.                                                   
 echo        -+-My Page Githup:https://github.com/mortzaLootTool-+-
+echo.
+echo       -+-Discord For Other Toolhttps://discord.gg/PASpgRWqSw-+-
 echo.  
 echo        ^|----------------------------------------------------^|
 echo.
 pause 
 goto :Home
+          
           
